c6c64ec5654aa66830100d72e29f3ad354d96075
Cristian Dolinta <cristian.sw@protonmail.com>
2021-06-12 19:13:11 +0000

    Homework 7 done.

---
+++ src/lesson7/Cat.java
@@ -0,0 +1,32 @@
+package lesson7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void eat(Plate p) {
+        satiety = p.decreaseFood(appetite);
+    }
+}
